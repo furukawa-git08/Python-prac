35dabb637aedf32a51d91f45403390b460741f05
furukawa <masaru.furukawa.d24@mail.toshiba>
2026-03-18 18:07:35 +0900

    UPDATE1

--- STEP2.py
+++ STEP2.py
@@ -1,3 +1,12 @@
-ron = [11.8, 12.2, 11.9, 12.5, 12.0, 11.7, 12.1, 12.3, 11.6, 12.4]
-print("avg =", sum(ron)/len(ron))
-print("min =", min(ron), "max =", max(ron))
+ron_list = [11.8, 12.2, 11.9, 12.5, 12.0, 11.7, 12.1, 12.3, 11.6, 12.4]
+S = 0
+
+
+for ron in ron_list:
+    if ron <=12.0:
+        S = S + 1
+
+
+
+print("suuryou =", S)
+#print("min =", min(ron), "max =", max(ron))
